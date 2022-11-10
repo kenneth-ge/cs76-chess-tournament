5f4b3a4fe4588f63849ec1839d6eec81df283649
Kenneth Ge <kennethkouge@gmail.com>
2022-11-09 19:06:16 -0500

    beats 1500 bot as long as it doesn't result in draw by repetition

--- src/ChessAI.cpp
+++ src/ChessAI.cpp
@@ -11,6 +11,31 @@
 #include <climits>
 #include <cassert>
 
+#include <sys/time.h>
+#include <iostream>
+
+#include <queue>
+
+struct timeval tp;
+long int current_time_millis(){
+	gettimeofday(&tp, NULL);
+	return tp.tv_sec * 1000 + tp.tv_usec / 1000;
+}
+
+inline long int last_time = 0;
+
+void start_timer(){
+	last_time = current_time_millis();
+}
+
+long end_timer(){
+	using namespace std;
+
+	long int current_time = current_time_millis();
+
+	return current_time - last_time;
+}
+
 using namespace std;
 using namespace thc;
 
@@ -37,20 +62,35 @@ const int MAX_DEPTH = 75;
 const int DEFAULT_MAX_DEPTH = 55;
 
 int value[128];
+int color[128];
 
 void init_values(){
-    value['P'] = 100;
-    value['p'] = -100;
-    value['Q'] = 800;
-    value['q'] = -800;
-    value['R'] = 500;
-    value['r'] = -500;
-    value['B'] = 300;
-    value['b'] = -300;
-    value['N'] = 300;
-    value['n'] = -300;
-    value['K'] = 1000000;
-    value['k'] = -1000000;
+    value['P'] = 1000;
+    value['p'] = -1000;
+    value['Q'] = 8000;
+    value['q'] = -8000;
+    value['R'] = 5000;
+    value['r'] = -5000;
+    value['B'] = 3000;
+    value['b'] = -3000;
+    value['N'] = 3000;
+    value['n'] = -3000;
+    value['K'] = 10000000;
+    value['k'] = -10000000;
+
+    color['P'] = 1;
+    color['p'] = -1;
+    color['Q'] = 1;
+    color['q'] = -1;
+    color['R'] = 1;
+    color['r'] = -1;
+    color['B'] = 1;
+    color['b'] = -1;
+    color['N'] = 1;
+    color['n'] = -1;
+    color['K'] = 1;
+    color['k'] = -1;
+    color[' '] = 0;
 }
 
 struct priority_move {
@@ -61,6 +101,22 @@ struct priority_move {
 	   return priority < another.priority;
 	}
 };
+
+bool out_of_bounds(int x, int y){
+	return x < 0 || y < 0 || x >= 8 || y >= 8;
+}
+
+int convert_to_num(int x, int y){
+	return x * 8 + y;
+}
+
+//-1 if black, 1 if white, 0 if none
+int black_or_white(char c){
+	return color[c];
+}
+
+bool attacker[2] = {false, true};
+int player[2] = {1, -1};
 
 int evaluate(ChessRules &board, int depth){
     TERMINAL eval_final_position;
@@ -77,9 +133,95 @@ int evaluate(ChessRules &board, int depth){
     }
 
     int eval = 0;
+    int material_value = 0;
 
     for(int i = 0; i < 64; i++){
         eval += value[board.squares[i]];
+        material_value += abs(value[board.squares[i]]);
+    }
+
+    //add things for check
+    eval -= board.AttackedPiece(board.wking_square) * 500;
+    eval += board.AttackedPiece(board.bking_square) * 500;
+
+    int squares[2] = {board.wking_square, board.bking_square};
+
+    if(abs(material_value) <= value['Q'] * 1.5){
+    	//endgame, count number of free squares the king has to move
+    	int dir[][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    	bool visited[64];
+
+    	for(int i = 0; i < 2; i++){
+    		fill(visited, visited + 64, false);
+			int free_squares = 1;
+
+			queue<pair<int, int>> q;
+
+			q.push({squares[i] / 8, squares[i] % 8});
+			visited[squares[i]] = true;
+
+			while(!q.empty()){
+				auto [r, c] = q.front();
+				q.pop();
+				for(int i = 0; i < 4; i++){
+					int nextR = r + dir[i][0];
+					int nextC = c + dir[i][1];
+
+					if(out_of_bounds(nextR, nextC))
+						continue;
+
+					int num = convert_to_num(nextR, nextC);
+
+					if(board.AttackedSquare(static_cast<Square>(num), attacker[i]))
+						continue;
+
+					if(visited[num])
+						continue;
+
+					visited[num] = true;
+					free_squares += 1;
+					q.push({nextR, nextC});
+				}
+			}
+
+			eval += free_squares * 15 * player[i];
+    	}
+    }
+
+    if(abs(material_value) >= value['Q']){
+        //king protection
+        //white king first
+        for(int k = 0; k < 2; k++){
+    		int loc[8][2];
+
+    		for(int i = 0; i < 8; i++){
+    			loc[i][0] = squares[k] / 8;
+    			loc[i][1] = squares[k] % 8;
+    		}
+
+    		int dir[8][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, +1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+
+    		//for each direction
+    		for(int i = 0; i < 8; i++){
+    			//move eight squares or until we hit a wall or get out of bounds
+    			for(int j = 0; j < 8; j++){
+    				loc[i][0] += dir[i][0];
+    				loc[i][1] += dir[i][1];
+
+    				if(out_of_bounds(loc[i][0], loc[i][1])){
+    					eval += -1 * black_or_white(squares[k]) * 4 * 5;
+    					break;
+    				}
+
+    				int val = black_or_white(board.squares[convert_to_num(loc[i][0], loc[i][1])]);
+
+    				if(val != 0){
+    					eval += val * (8 - j) * 5;
+    					break;
+    				}
+    			}
+    		}
+        }
     }
 
     return eval;
@@ -235,13 +377,39 @@ thc::Move choose_move(ChessRules &board){
 int main() {
     init_values();
 
-	ChessPosition cb;
-	bool works = cb.Forsyth("3k4/6p1/3K1pPp/3PrP2/p7/P7/3Q3P/1q6 w - - 0 2");
-	assert(works);
-	ChessRules cr(cb);
+    long total_time = 0;
 
-	display_position(cr, "Starting position");
+    while(true){
+		std::string str;
+		std::getline(std::cin, str);
 
+		ChessPosition cb;
+		bool works = cb.Forsyth(str.c_str());
+		assert(works);
+		ChessRules cr(cb);
+
+		display_position(cr, "Starting position");
+
+		max_depth_reached = 0;
+
+		start_timer();
+		Move best_move = choose_move(cr);
+		long time = end_timer();
+		total_time += time;
+
+		cout << "time " << time << endl;
+		cout << "total time " << total_time << endl;
+
+		cout << "max depth " << max_depth_reached << endl;
+
+		cout << "terse out " << best_move.TerseOut() << endl;
+		cout << "natural out " << best_move.NaturalOut(&cr) << endl;
+    }
+
+	//cr.PlayMove(best_move);
+	//display_position(cr, "Next Move");
+
+	/*
 	while(true){
 		Move best_move = choose_move(cr);
 
@@ -258,7 +426,7 @@ int main() {
 
 	cout << "GAME OVER" << endl;
 
-	cout << "max depth reached " << max_depth_reached << endl;
+	cout << "max depth reached " << max_depth_reached << endl;*/
 
 	return 0;
 }
